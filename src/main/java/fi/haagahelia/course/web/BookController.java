cda4b3a0f5b5161bc3a30c65d26d63db9d93f03e
Abhishek chhetri <abhichhetri@outlook.com>
2023-09-27 22:24:23 +0300

    REST created which returns all books

--- src/main/java/fi/haagahelia/course/web/BookController.java
+++ src/main/java/fi/haagahelia/course/web/BookController.java
@@ -1,22 +1,58 @@
 package fi.haagahelia.course.web;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import fi.haagahelia.course.domain.Book;
 import fi.haagahelia.course.domain.BookRepository;
 import fi.haagahelia.course.domain.CategoryRepository;
 
-@Controller
+@RestController
 public class BookController {
 	@Autowired
 	private BookRepository repository; 
     @Autowired
     private CategoryRepository categoryRepository;
+    
+    @RequestMapping(value= {"/", "/api/books"})
+    public List<Book> getAllBooks() {
+    	Iterable<Book> bookIterable = repository.findAll();
+        List<Book> books = new ArrayList<>();
+    	bookIterable.forEach(books::add);
+
+        books.forEach(book -> {
+            book.setCategory(null); 
+        });
+
+        return books;
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        Optional<Book> optionalBook = repository.findById(id);
+        
+        if (optionalBook.isPresent()) {
+            Book book = optionalBook.get();
+            book.setCategory(null);
+            
+            return ResponseEntity.ok(book);
+        } else {
+            // Handle book not found error here
+            // You can throw an exception or return a specific response
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 	
     @RequestMapping(value= {"/", "/booklist"})
     public String studentList(Model model) {	
@@ -42,4 +78,5 @@ public class BookController {
     	repository.deleteById(id);
         return "redirect:../booklist";
     }     
+   
 }
